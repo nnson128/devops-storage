3568d1cc43343c624c8f7980258c24bf62c59978
Nguyễn Ngọc Sơn <143249343+nnson128@users.noreply.github.com>
2025-05-08 22:42:02 +0700

    Update private-registry.sh

--- tools-install/private-registry.sh
+++ tools-install/private-registry.sh
@@ -2,7 +2,7 @@ apt get update
 apt-get install openssl
 mkdir -p /tools/registry/ && cd /tools/registry
 mkdir data certs
-openssl req -newkey rsa:4096 -nodes -sha256 -keyout certs/domain.key -subj "/CN=192.168.1.100" -addext "subjectAltName = DNS:192.168.1.100,IP:192.168.1.100" -x509 -days 365 -out certs/domain.crt
+openssl req -newkey rsa:4096 -nodes -sha256 -keyout certs/domain.key -subj "/CN=192.168.227.100" -addext "subjectAltName = DNS:192.168.227.100,IP:192.168.227.100" -x509 -days 365 -out certs/domain.crt
 vi docker-compose.yml
 
 
