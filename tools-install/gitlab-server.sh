8e4b92fb7c6d5dba482fa083851f441ebf817db6
NGUYEN NGOC SON <sonvipkl04@gmail.com>
2025-05-09 13:01:39 +0700

    .

--- tools-install/gitlab-server.sh
+++ tools-install/gitlab-server.sh
@@ -1,6 +1,19 @@
+### Gitlab server install and configure ###
 # install gitlab server
 
 curl -s https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
-
 sudo apt-get install gitlab-ee=14.4.1-ee.0 -y
+
+# gitlab server
+vi /etc/hosts: 192.168.227.100 | git.nnson128.tech
+vi /etc/gitlab/gitlab.rb | external_url 'http://git.nnson128.tech'
+gitlab-ctl reconfigure
+root - vi /etc/gitlab/init-password 
+
+# macos server
+macos: 
+vi /etc/hosts | 192.168.227.100 git.nnson128.tech
+
+# lab server
+vi /etc/hosts: 192.168.227.100 | git.nnson128.tech
 
--- tools-install/private-registry.sh
+++
@@ -1,8 +0,0 @@
-apt update
-apt install openssl
-mkdir -p /tools/registry/ && cd /tools/registry
-mkdir data certs
-openssl req -newkey rsa:4096 -nodes -sha256 -keyout certs/domain.key -subj "/CN=192.168.227.100" -addext "subjectAltName = DNS:192.168.227.100,IP:192.168.227.100" -x509 -days 365 -out certs/domain.crt
-vi docker-compose.yml
-
-
