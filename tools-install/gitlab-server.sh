00a8f3c3c831807dc073970dc3a1b048ee581dae
NGUYEN NGOC SON <sonvipkl04@gmail.com>
2025-05-09 16:12:10 +0700

    .

--- tools-install/gitlab-server.sh
+++ tools-install/gitlab-server.sh
@@ -5,10 +5,10 @@ curl -s https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script
 sudo apt-get install gitlab-ee=14.4.1-ee.0 -y
 
 # gitlab server
-vi /etc/hosts: 192.168.227.100 | git.nnson128.tech
+vi /etc/hosts: 192.168.227.100 git.nnson128.tech
 vi /etc/gitlab/gitlab.rb | external_url 'http://git.nnson128.tech'
 gitlab-ctl reconfigure
-vi /etc/gitlab/initial_root_password | 88888888
+cat /etc/gitlab/initial_root_password | 88888888
 
 # macos server
 macos: 
