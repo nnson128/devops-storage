4c22d41a9c89e45a2a07bff641e1873e86d77043
NGUYEN NGOC SON <sonvipkl04@gmail.com>
2025-05-09 16:52:37 +0700

    .

--- tools-install/gitwork-flow.sh
+++ tools-install/gitwork-flow.sh
@@ -3,28 +3,26 @@
     - develop
     - feature/frontend/login
     - feature/backend/login
-2. authorize user
-    - setting -> repositories -> default branch -> develop
-    "protected branch"
-    - main: maintainer - maintainer
-    - develop: maintainer - maintainer | devops: commit config(database): modify connection
+    - staging
+2. protected branches
+    - default branch -> develop
+
+    - main: maintainer - no one
+    - develop: maintainer - maintainer
+        + devops: commit config(database): modify connection
     - staging: maintainer - maintainer
-    - push commit to main || cannot push commit to main
-# 3. create tags: Đánh dấu phiên bản triển khai dự án
-    - develop branch: 
-        + Repo -> tags 
-            + name: dev_0.0.1
-            + ``` create tag
+    - push commit to main 
+        + cannot push commit to main
 
-
-
-2. create user dev1
-    - invite dev1
-88888888
-# user dev1
+3. create user dev1
     - git commit
     - new merge request
     - assign: leader 
     - reviewer: leader
     - close "Delete source branch when merge request is accepted"
     ``` create merge request
+4. create tags: Đánh dấu phiên bản triển khai dự án
+    - develop branch: 
+        + Repo -> tags 
+            + name: dev_0.0.1
+            + ``` create tag
