d5d8f47a0873c2b0b65f645eaa24feb0055a9efd
NGUYEN NGOC SON <sonvipkl04@gmail.com>
2025-05-12 16:59:43 +0700

    .

--- jenkins/install-jenkins.sh
+++ jenkins/install-jenkins.sh
@@ -1,12 +1,32 @@
-#!/bin/bash
-sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
-  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt-get install jenkins
+- change netplan
+- vi /etc/hostname -> netplan apply -> reboot
+___ install jenkins.sh ___
+chmod +x install_jenkins.sh
 
-# install java
-apt-get upgrade
-apt install openjdk-17-jdk openjdk-17-jre
+#!/bin/bash
+[ "$EUID" -ne 0 ] && exit 1
+sed -i '/cdrom/d' /etc/apt/sources.list
+apt-get update --fix-missing
+apt-get install -y openjdk-17-jdk openjdk-17-jre || {
+  add-apt-repository ppa:openjdk-r/ppa -y
+  apt-get update
+  apt-get install -y openjdk-17-jdk openjdk-17-jre
+}
+java -version || exit 1
+grep -q JAVA_HOME /etc/environment || echo "JAVA_HOME=/usr/lib/jvm/java-17-openjdk-arm64" >> /etc/environment
+source /etc/environment
+[ -f /usr/sbin/policy-rc.d ] && mv /usr/sbin/policy-rc.d /usr/sbin/policy-rc.d.bak
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | apt-key add -
+sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 5BA31D57EF5975CA
+apt-get update
+apt-get install -y jenkins
+systemctl daemon-reload
+systemctl start jenkins
+systemctl enable jenkins
+ufw allow 8080
+
+
+
+___ install nginx và cho jenkins chạy trên cổng 80 ___
+vi /etc/nginx/conf.d/jenkins.nnson128.tech.conf
