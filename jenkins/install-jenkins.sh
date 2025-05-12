54518c8cc21f9314bd55048b4e10f701c03f9717
NGUYEN NGOC SON <sonvipkl04@gmail.com>
2025-05-12 11:17:09 +0700

    .

---
+++ jenkins/install-jenkins.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins
+
+# install java
+apt-get upgrade
+apt install openjdk-17-jdk openjdk-17-jre
---
+++ nginx/deploy-fe.sh
@@ -0,0 +1,5 @@
+` vi /etc/nginx/conf.d/todolist.conf
+
+` usermod -aG group_name nginx_name
+
+` nginx -s reload
