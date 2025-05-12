086c6cf0d98dc95aff75dc0ff98de293eb38243a
NGUYEN NGOC SON <sonvipkl04@gmail.com>
2025-05-12 11:36:59 +0700

    .

--- nginx/deploy-fe-service.sh
+++ nginx/deploy-fe-service.sh
@@ -1,6 +1,13 @@
+### create
 ` vi /lib/systemd/system/vision.service
 FILE_AT: ./vision.service.sh
 
 ` systemctl daemon-reload
 
 ` systemctl status vision
+
+
+### remove
+sudo systemctl stop <service>
+sudo systemctl disable <service>
+sudo systemctl daemon-reload   # reload systemd
