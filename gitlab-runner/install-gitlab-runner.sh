657583d34b461dbafcb5844c0ef8511c16794f18
NGUYEN NGOC SON <sonvipkl04@gmail.com>
2025-05-15 13:40:26 +0700

    .

--- gitlab-runner/install-gitlab-runner.sh
+++ gitlab-runner/install-gitlab-runner.sh
@@ -1,11 +1,11 @@
-___ install and config gitlab-runner ___
-I. install gitlab-runner
-``` apt-get update
-``` curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | bash
-``` apt-get install gitlab-runner
-``` gitlab-runner -version
-``` user vi /etc/passwd
-
+1. install gitlab-runner
+```
+apt-get update
+curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | bash
+apt-get install gitlab-runner
+gitlab-runner -version
+user vi /etc/passwd
+```
 II. connect to gitlab server
 - access git.nnson128.tech
 - settings -> CI/CD -> runner
