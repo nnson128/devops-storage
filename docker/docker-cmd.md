eb9b3916b162c77828222512a7dee658ab5d5c8c
NGUYEN NGOC SON <sonvipkl04@gmail.com>
2025-05-15 15:11:09 +0700

    .

---
+++ docker/docker-cmd.md
@@ -0,0 +1,3 @@
+```
+docker tag todolist:v1 registry.nnson128.io.vn/elroy(project)/todolist:v1
+```
--- docker/docker-command.sh
+++
@@ -1,8 +0,0 @@
-` docker run --name container_name -dp 8888:8080 image_name:v1
-
-` docker rm -f $(docker ps -aq)
-
-` docker system df
-
-` docker tag todolist:v1 sonta28122004/todolist:v1
-
