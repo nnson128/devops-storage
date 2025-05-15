201b41b96338dd2c74eb759eb4e74ebbde639457
NGUYEN NGOC SON <sonvipkl04@gmail.com>
2025-05-15 13:37:27 +0700

    .

--- git/base-practise.sh
+++ git/base-practise.sh
@@ -1,18 +1,23 @@
 ### base practise ###
-___ push project from lab server ___
-
+1. push project from lab server ___
+```
 # Create group
 1. create group | group includes subgroups || projects || microservices || Frontend || Backend
     - private
     - role: devops engineer
     - just me
     ``` create group
+```
+```
 2. new projects 
     - project name: shoeshop
     - close "Initialize repository with a README"
     ``` create project
+```
+```
 # lab server
 1. git configure
     - git clone http://git.nnson128.tech/shoeshop/shoeshop.git
     - cp -rf /projects/shoeshop/* /data/shoeshop
     - git push -f origin
+```
